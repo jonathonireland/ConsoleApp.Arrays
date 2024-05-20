0384d6d1231096841e5a9c8651dc9f3996462f82
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-20 17:08:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("****************** - Arrays - *******************");
+
+// Declare Fixed Size Array
+
+// Add values to Fixed Size Array
+
+// Print values in Fixedc Size Array
+
+// Declare Variable Sized Array 
+
+// Add values to Variable Sized Array
+
+// Print values in Variable Sized Array
+
